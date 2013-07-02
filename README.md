f89059162526a1a7a46ddff665b3c1daece1d60f
Leeroy Brun <leeroy.brun@POSTE-LEEROY.batiplus.local>
2013-07-02 09:44:22 +0200

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 amazon-glacier-removal
 ======================
 
-Tool to remove all archives stored inside an Amazon Glacier vault.
+Tool to remove all archives stored inside an Amazon Glacier vault. It will then remove the vault too.
+
+This requires boto, you can install it like this :
+
+```shell
+pip install boto
+```
+
+Then create a `credentials.json` file in the script directory :
+
+```json
+{
+	"AWSAccessKeyId": "YOURACCESSKEY",
+	"AWSSecretKey":   "YOURSECRETKEY"
+}
+```
+
+You can then use the script like this :
+
+```shell
+python .\removeVault.py REGION-NAME VAULT-NAME
+```
+
+Example :
+
+```shell
+python .\removeVault.py eu-west-1 my_vault
+```
---
+++ removeVault.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# -*- coding: UTF-8 -*-
+
+import sys
+import json
+import time
+import boto.glacier
+
+# Get arguments
+regionName = sys.argv[1]
+vaultName = sys.argv[2]
+
+# Load credentials
+config = json.loads(open('credentials.json').read())
+
+print 'Connect to Amazon Glacier...'
+
+glacier = boto.glacier.connect_to_region(regionName, aws_access_key_id=config['AWSAccessKeyId'], aws_secret_access_key=config['AWSSecretKey'])
+
+print 'Get selected vault...'
+vault = glacier.get_vault(vaultName)
+
+print 'Initiate inventory retrieval job...'
+jobID = vault.retrieve_inventory(description='Python Amazon Glacier Removal Tool')
+
+# Get job status
+job = vault.get_job(jobID)
+
+while job.status_code == 'InProgress':
+	print 'Inventory not ready, sleep for 30 mins...'
+
+	time.sleep(60*30)
+
+	job = vault.get_job(jobID)
+
+if job.status_code == 'Succeeded':
+	print 'Inventory retrieved, parsing data...'
+	inventory = json.loads(job.get_output())
+
+	print 'Loop over archives...'
+	for archive in inventory['ArchiveList']:
+		print 'Remove archive ID : '+ archive['ArchiveId']
+		vault.delete_archive(archive['ArchiveId'])
+
+	print 'Vault removed !'
+
+else:
+	print 'Vault retrieval failed...'
