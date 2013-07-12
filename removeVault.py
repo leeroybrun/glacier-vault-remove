d6e359eacbd0b229499282388badf8cacec30463
Leeroy Brun <leeroy.brun@POSTE-LEEROY.batiplus.local>
2013-07-12 11:05:56 +0200

    Updated .gitignore & try to sleep when an error occur when trying to remove archive

--- removeVault.py
+++ removeVault.py
@@ -63,6 +63,12 @@ if job.status_code == 'Succeeded':
 			except:
 				print 'Error : ', sys.exc_info()[0]
 
+				print 'Sleep 30 mins before retrying...'
+				time.sleep(60*30)
+
+				print 'Retry to remove archive ID again : '+ archive['ArchiveId']
+				vault.delete_archive(archive['ArchiveId'])
+
 	print 'Removing vault...'
 	vault.remove()
 
