101be63b1c8eca2f5da190fa7f9dc8d3debdcf7f
Leeroy Brun <leeroy.brun@gmail.com>
2015-11-27 14:12:31 +0100

    Better exceptions messages

--- removeVault.py
+++ removeVault.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+﻿#!/usr/bin/env python
 
 # -*- coding: UTF-8 -*-
 
@@ -9,6 +9,10 @@ import os.path
 import logging
 import boto.glacier
 from socket import gethostbyname, gaierror
+
+def printException():
+	exc_type, exc_value = sys.exc_info()[:2]
+	logging.error('Exception "%s" occured with message "%s"', exc_type.__name__, exc_value)
 
 # Default logging config
 logging.basicConfig(format='%(levelname)s : %(message)s', level=logging.INFO)
@@ -28,33 +32,40 @@ if len(sys.argv) == 4 and sys.argv[3] == 'DEBUG':
 	logging.basicConfig(level=logging.DEBUG)
 
 # Load credentials
-f = open('credentials.json', 'r')
-config = json.loads(f.read())
-f.close()
+try:
+	f = open('credentials.json', 'r')
+	config = json.loads(f.read())
+	f.close()
+except:
+	logging.error('Cannot load "credentials.json" file...')
+	printException()
+	sys.exit(1)
 
 try:
 	logging.info('Connecting to Amazon Glacier...')
 	glacier = boto.glacier.connect_to_region(regionName, aws_access_key_id=config['AWSAccessKeyId'], aws_secret_access_key=config['AWSSecretKey'])
 except:
-	logging.error(sys.exc_info()[0])
+	printException()
 	sys.exit(1)
 
 if vaultName == 'LIST':
-    try:
-        logging.info('Getting list of vaults...')
-        vaults = glacier.list_vaults()
-    except:
-        logging.error(sys.exc_info()[0])
-        sys.exit(1)
-    for vault in vaults:
-        logging.info(vault.name)
-    exit(0)
+	try:
+		logging.info('Getting list of vaults...')
+		vaults = glacier.list_vaults()
+	except:
+		printException()
+		sys.exit(1)
+
+	for vault in vaults:
+		logging.info(vault.name)
+
+	exit(0)
 
 try:
 	logging.info('Getting selected vault...')
 	vault = glacier.get_vault(vaultName)
 except:
-	logging.error(sys.exc_info()[0])
+	printException()
 	sys.exit(1)
 
 logging.info('Getting jobs list...')
@@ -71,8 +82,8 @@ if jobID == '':
 	logging.info('No existing job found, initiate inventory retrieval...')
 	try:
 		jobID = vault.retrieve_inventory(description='Python Amazon Glacier Removal Tool')
-	except Exception as e:
-		logging.error(e)
+	except:
+		printException()
 		sys.exit(1)
 
 logging.debug('Job ID : %s', jobID)
@@ -97,8 +108,8 @@ if job.status_code == 'Succeeded':
 			logging.debug('Remove archive ID : %s', archive['ArchiveId'])
 			try:
 				vault.delete_archive(archive['ArchiveId'])
-			except Exception as e:
-				logging.error(e)
+			except:
+				printException()
 
 				logging.info('Sleep 2 mins before retrying...')
 				time.sleep(60*2)
@@ -115,6 +126,7 @@ if job.status_code == 'Succeeded':
 		vault.delete()
 		logging.info('Vault removed.')
 	except:
+		printException()
 		logging.error("We can't remove the vault now. Please wait some time and try again. You can also remove it from the AWS console, now that all archives have been removed.")
 
 else:
