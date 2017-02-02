1ce80662f5629b1db2cb3600ffac8fe5ce0857c2
Raphaël Riel <raphael.riel@gmail.com>
2017-02-02 10:20:08 -0500

    Fix dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires = ['3>=1.4.4'],
+    install_requires = ['boto3>=1.4.4'],
 
     # metadata for upload to PyPI
     author = "Leeroy Brun",
